1a227c31756b3ca0c2192753a305518dc0324d4a
ciro <csayagues@eagerworks.com>
2022-12-05 14:26:41 -0300

    dynamodb example divided in accesor and definer

---
+++ function_dynamodb/dynamodb-definer/function.js
@@ -0,0 +1,24 @@
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const handler = async () => {
+  const db = new AWS.DynamoDB.DocumentClient(
+    JSON.parse(process.env.DYNAMO_OPTIONS ?? "{}")
+  );
+
+  console.log("dynamo options", process.env.DYNAMO_OPTIONS ?? "{}");
+
+  const param = {
+    TableName: process.env.TABLE_NAME,
+  };
+
+  const output = await db.scan(param).promise();
+  return {
+    body: JSON.stringify(output.Items),
+    statusCode: 200,
+  };
+};
+
+export default handler;
